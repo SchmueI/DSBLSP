1bb7e5d34e373fa3717f51de071dea85b12d1e68
Schmuel Odradek <git@schmuel.net>
2021-12-31 13:16:50 +0100

    Test socket.io

--- app.js
+++ app.js
@@ -10,3 +10,9 @@ app.get("/", function(req, res){
 app.use("/client", express.static(__dirname + "/client"))
 
 serv.listen(2000)
+console.log("Server started at Port 2000")
+
+var io = require("socket.io")(serv,{});
+io.sockets.on("connection", function(socket){
+    console.log("Socket connection")
+})
