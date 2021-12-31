fcebbea6da70cd74da833cb7b470efa199c6e02b
Schmuel Odradek <git@schmuel.net>
2021-12-31 01:00:45 +0100

    Set up express module and test server state

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("NodeJS and NPM install succeeded");
+var express = require("express");
+var app = express();
+var serv = require("http").Server(app);
+
+app.get("/", function(req, res){
+
+    res.sendFile(__dirname + "/client/index.html");
+});
+
+app.use("/client", express.static(__dirname + "/client"))
+
+serv.listen(2000)
