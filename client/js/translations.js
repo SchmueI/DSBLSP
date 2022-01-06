b24e202ad6202c45447a4ecd7444e23abc48d99f
Schmuel Odradek <dev@schmuel.net>
2022-01-06 23:33:54 +0100

    Include translation files and add german translation

---
+++ client/js/translations.js
@@ -0,0 +1,55 @@
+function translate_german(){
+    document.getElementById("textTitle").innerHTML="Digitales Schwarzes Brett";
+    document.getElementById("welcomeText").innerHTML="Willkommen";
+    document.getElementById("introText").innerHTML="Dies ist das DSB Dashboard. Du kannst die Software jetzt benutzen! Wenn Du Schwierigkeiten mit dem Start hast, schau Dir den \"Anleitung\" Bereich an. Keine Sorge, es ist sehr eifnach!! " 
+    document.getElementById("headText").innerHTML="Live Übertragung";
+    document.getElementById("messageText").innerHTML="Nachricht";
+    document.getElementById("msg").placeholder="Nachricht eingeben";
+    document.getElementById("durText").innerHTML="Anzeigedauer";
+    document.getElementById("onedayText").innerHTML="Einen Tag lang anzeigen";
+    document.getElementById("onehalfdayText").innerHTML="Anderthalb Tage lang anzeigen";
+    document.getElementById("twodaysText").innerHTML="Zwei Tage lang anzeigen";
+    document.getElementById("twohalfdayText").innerHTML="Zweieinhalb Tage lang anzeigen";
+    document.getElementById("threedayText").innerHTML="Drei Tage lang anzeigen";
+    document.getElementById("weekText").innerHTML="Eine Woche lang anzeigen";
+    document.getElementById("sizeText").innerHTML="Schriftgröße";
+    document.getElementById("smallText").innerHTML="Klein";
+    document.getElementById("normText").innerHTML="Normal";
+    document.getElementById("greatText").innerHTML="Groß";
+    document.getElementById("greatestText").innerHTML="Überschrift";
+    document.getElementById("schedText").innerHTML="Planen";
+    document.getElementById("nowText").innerHTML="Jetzt senden";
+    document.getElementById("sched1hourText").innerHTML="In einer Stunde senden";
+    document.getElementById("sched12hourText").innerHTML="In zwölf Stunden senden";
+    document.getElementById("sched1dayText").innerHTML="In einem Tag senden";
+    document.getElementById("sched2dayText").innerHTML="In zwei Tagen senden";
+    document.getElementById("sched3dayText").innerHTML="In drei Tagen senden";
+    document.getElementById("sched1weekText").innerHTML="In einer Woche senden";
+    document.getElementById("sendText").innerHTML="Senden";
+    document.getElementById("advancedText").innerHTML="Erweitert";
+    document.getElementById("actionText").innerHTML="Erweiterte Funktionen";
+    document.getElementById("ereaseText").innerHTML="Alles löschen";
+    document.getElementById("ereaseDescText").innerHTML="Mit diesem Knopf wird die gesamte Tafel gesäubert.";
+    document.getElementById("deleteText").innerHTML="Letzten Eintrag löschen";
+    document.getElementById("deleteDescText").innerHTML="Mit diesem Knopf wird die zuletzt gesendete Nachricht gelöscht.";
+    document.getElementById("howtoText").innerHTML="Anleitung";
+    document.getElementById("howdescText").innerHTML=""+
+        "👋&emsp;Willkommen beim Schwarzen Brett! Diese Software soll so einfach wie möglich sein. <br>"+
+        "💌&emsp;Du hast eine Nachricht zu teilen? Super! Gib sie einfach ein und klick \"Senden\". <br>"+
+		"👀&emsp;Die Mitteilung wird sofort für jeden zu sehen sein.<br>"+
+		"🔧&emsp;Du kannst die Nachricht mit gedrückter Maustaste auf dem Bildschirm verschieben. <br>"+
+		"🗑️&emsp;Mit einem Doppelklick werden Nachrichten wieder entfernt.<br>"+
+		"📅&emsp;Wenn die Nachricht nicht manuell gelöscht wird, passiert das automatisch nach zwei Tagen.";
+    document.getElementById("startText").innerHTML="🚀&emsp;Das wars! Du kannst sofort loslegen!";
+    document.getElementById("moreText").innerHTML="Du willst mehr?";
+    document.getElementById("moredescText").innerHTML=""+
+        "🤹&emsp;Wenn Du noch mehr Einstellungen willst, hab ich da was für dich: <br>"+
+        "🦚&emsp;Klick einfach auf \"Erweitert\" unterhalb des Senden-Knopfes um alle Einstellungen zu sehen<br>"+
+        "📚&emsp;Wenn Du Hilfe brauchst, schau Dir die folgenden Artikel an.;"
+    document.getElementById("scheduleTitleText").innerHTML="Nachrichten Planen";
+    document.getElementById("scheddescText").innerHTML="Dieser Artikel wird Dir zeigen, wie Du den Zeitraum deiner Nachrichten kontrollieren kannst.";
+    document.getElementById("learn1more").innerHTML="Mehr erfahren";
+    document.getElementById("timetableTitleText").innerHTML="Tagespläne bearbeiten";
+    document.getElementById("timetabledescText").innerHTML="Lerne wie Du die wöchentlichen Informationen des Tagesplans auf der rechten Seite bearbeitest.";
+    document.getElementById("learn2more").innerHTML="Mehr erfahren";
+}
