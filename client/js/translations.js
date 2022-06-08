8752ef698f190784d80e4984adfec72f4f34fca6
Schmuel Odradek <dev@schmuel.net>
2022-06-08 11:37:10 +0200

    refactored translation script

--- client/js/translations.js
+++ client/js/translations.js
@@ -1,73 +1,73 @@
 function index_translate_german(){
-    document.getElementById("textTitle").innerHTML="Digitales Schwarzes Brett";
-    document.getElementById("welcomeText").innerHTML="Willkommen";
-    document.getElementById("introText").innerHTML="Dies ist das DSB Dashboard. Du kannst die Software jetzt benutzen! Wenn Du Schwierigkeiten mit dem Start hast, schau Dir den \"Anleitung\" Bereich an. Keine Sorge, es ist sehr eifnach! " 
-    document.getElementById("Login-Head").innerHTML="Anmelden";
-    document.getElementById("Login-Explain").innerHTML=""+
+    if (document.querySelector("#textTitle")) document.getElementById("textTitle").innerHTML="Digitales Schwarzes Brett";
+    if (document.querySelector("#welcomeText")) document.getElementById("welcomeText").innerHTML="Willkommen";
+    if (document.querySelector("#Login-Text")) document.getElementById("introText").innerHTML="Dies ist das DSB Dashboard. Du kannst die Software jetzt benutzen! Wenn Du Schwierigkeiten mit dem Start hast, schau Dir den \"Anleitung\" Bereich an. Keine Sorge, es ist sehr eifnach! " 
+    if (document.querySelector("#Login-Head")) document.getElementById("Login-Head").innerHTML="Anmelden";
+    if (document.querySelector("#Login-Explain")) document.getElementById("Login-Explain").innerHTML=""+
         "🔒&emsp;Um das DSB Dashboard benutzen zu können, musst du dich als Nutzer anmelden.<br>"+
         "🔑&emsp;Bitte gib die Daten ein, die dir dein Administrator zur Verfügung gestellt hat.<br>"+
 		"🧙🏼‍♂️&emsp;Wenn du noch keine Zugangsdaten erhalten hast, oder deine Daten verlegt hast, wende dich an den Administrator.";
-    document.getElementById("username-label").innerHTML="Nutzername";
-    document.getElementById("usrname").placeholder="👤 Nutzername";
-    document.getElementById("password-label").innerHTML="Passwort";
-    document.getElementById("pssword").placeholder="🔑 Passwort";
-    document.getElementById("Login-Submit").innerHTML="Anmelden"
-    document.getElementById("headText").innerHTML="Live Übertragung";
-    document.getElementById("messageText").innerHTML="Nachricht";
-    document.getElementById("msg").placeholder="Nachricht eingeben";
-    document.getElementById("durText").innerHTML="Anzeigedauer";
-    document.getElementById("onedayText").innerHTML="Einen Tag lang anzeigen";
-    document.getElementById("onehalfdayText").innerHTML="Anderthalb Tage lang anzeigen";
-    document.getElementById("twodaysText").innerHTML="Zwei Tage lang anzeigen";
-    document.getElementById("twohalfdayText").innerHTML="Zweieinhalb Tage lang anzeigen";
-    document.getElementById("threedayText").innerHTML="Drei Tage lang anzeigen";
-    document.getElementById("weekText").innerHTML="Eine Woche lang anzeigen";
-    document.getElementById("sizeText").innerHTML="Schriftgröße";
-    document.getElementById("smallText").innerHTML="Klein";
-    document.getElementById("normText").innerHTML="Normal";
-    document.getElementById("greatText").innerHTML="Groß";
-    document.getElementById("greatestText").innerHTML="Überschrift";
-    document.getElementById("schedText").innerHTML="Planen";
-    document.getElementById("nowText").innerHTML="Jetzt senden";
-    document.getElementById("sched1hourText").innerHTML="In einer Stunde senden";
-    document.getElementById("sched12hourText").innerHTML="In zwölf Stunden senden";
-    document.getElementById("sched1dayText").innerHTML="In einem Tag senden";
-    document.getElementById("sched2dayText").innerHTML="In zwei Tagen senden";
-    document.getElementById("sched3dayText").innerHTML="In drei Tagen senden";
-    document.getElementById("sched1weekText").innerHTML="In einer Woche senden";
-    document.getElementById("sendText").innerHTML="Senden";
-    document.getElementById("advancedText").innerHTML="Erweitert";
-    document.getElementById("timetableconfigText").innerHTML="Tagespläne konfigurieren"
-    document.getElementById("sundayText").innerHTML="Sonntag";
-    document.getElementById("mondayText").innerHTML="Montag";
-    document.getElementById("tuesdayText").innerHTML="Dienstag";
-    document.getElementById("wednesdayText").innerHTML="Mittwoch";
-    document.getElementById("thursdayText").innerHTML="Donnerstag";
-    document.getElementById("fridayText").innerHTML="Freitag";
-    document.getElementById("saturdayText").innerHTML="Samstag";
-    document.getElementById("actionText").innerHTML="Erweiterte Funktionen";
-    document.getElementById("ereaseText").innerHTML="Alles löschen";
-    document.getElementById("ereaseDescText").innerHTML="Mit diesem Knopf wird die gesamte Tafel gesäubert.";
-    document.getElementById("deleteText").innerHTML="Letzten Eintrag löschen";
-    document.getElementById("deleteDescText").innerHTML="Mit diesem Knopf wird die zuletzt gesendete Nachricht gelöscht.";
-    document.getElementById("howtoText").innerHTML="Anleitung";
-    document.getElementById("howdescText").innerHTML=""+
+    if (document.querySelector("#username-labe")) document.getElementById("username-label").innerHTML="Nutzername";
+    if (document.querySelector("#usrname")) document.getElementById("usrname").placeholder="👤 Nutzername";
+    if (document.querySelector("#password-label")) document.getElementById("password-label").innerHTML="Passwort";
+    if (document.querySelector("#pssword")) document.getElementById("pssword").placeholder="🔑 Passwort";
+    if (document.querySelector("#Login-Submit")) document.getElementById("Login-Submit").innerHTML="Anmelden"
+    if (document.querySelector("#headText")) document.getElementById("headText").innerHTML="Live Übertragung";
+    if (document.querySelector("#messageText")) document.getElementById("messageText").innerHTML="Nachricht";
+    if (document.querySelector("#msg")) document.getElementById("msg").placeholder="Nachricht eingeben";
+    if (document.querySelector("#durText")) document.getElementById("durText").innerHTML="Anzeigedauer";
+    if (document.querySelector("#onedayText")) document.getElementById("onedayText").innerHTML="Einen Tag lang anzeigen";
+    if (document.querySelector("#onehalfdayText")) document.getElementById("onehalfdayText").innerHTML="Anderthalb Tage lang anzeigen";
+    if (document.querySelector("#twodaysText")) document.getElementById("twodaysText").innerHTML="Zwei Tage lang anzeigen";
+    if (document.querySelector("#twohalfdayText")) document.getElementById("twohalfdayText").innerHTML="Zweieinhalb Tage lang anzeigen";
+    if (document.querySelector("#threedayText")) document.getElementById("threedayText").innerHTML="Drei Tage lang anzeigen";
+    if (document.querySelector("#weekText")) document.getElementById("weekText").innerHTML="Eine Woche lang anzeigen";
+    if (document.querySelector("#sizeText")) document.getElementById("sizeText").innerHTML="Schriftgröße";
+    if (document.querySelector("#normText")) document.getElementById("smallText").innerHTML="Klein";
+    if (document.querySelector("#normText")) document.getElementById("normText").innerHTML="Normal";
+    if (document.querySelector("#greatText")) document.getElementById("greatText").innerHTML="Groß";
+    if (document.querySelector("#greatestText")) document.getElementById("greatestText").innerHTML="Überschrift";
+    if (document.querySelector("#schedText")) document.getElementById("schedText").innerHTML="Planen";
+    if (document.querySelector("#nowText")) document.getElementById("nowText").innerHTML="Jetzt senden";
+    if (document.querySelector("#sched1hourText")) document.getElementById("sched1hourText").innerHTML="In einer Stunde senden";
+    if (document.querySelector("#sched12hourText")) document.getElementById("sched12hourText").innerHTML="In zwölf Stunden senden";
+    if (document.querySelector("#sched1dayText")) document.getElementById("sched1dayText").innerHTML="In einem Tag senden";
+    if (document.querySelector("#sched2dayText")) document.getElementById("sched2dayText").innerHTML="In zwei Tagen senden";
+    if (document.querySelector("#sched3dayText")) document.getElementById("sched3dayText").innerHTML="In drei Tagen senden";
+    if (document.querySelector("#sched1weekText")) document.getElementById("sched1weekText").innerHTML="In einer Woche senden";
+    if (document.querySelector("#sendText")) document.getElementById("sendText").innerHTML="Senden";
+    if (document.querySelector("#advancedText")) document.getElementById("advancedText").innerHTML="Erweitert";
+    if (document.querySelector("#timetableconfigText")) document.getElementById("timetableconfigText").innerHTML="Tagespläne konfigurieren"
+    if (document.querySelector("#sundayText")) document.getElementById("sundayText").innerHTML="Sonntag";
+    if (document.querySelector("#mondayText")) document.getElementById("mondayText").innerHTML="Montag";
+    if (document.querySelector("#tuesdayText")) document.getElementById("tuesdayText").innerHTML="Dienstag";
+    if (document.querySelector("#wednesdayText")) document.getElementById("wednesdayText").innerHTML="Mittwoch";
+    if (document.querySelector("#thursdayText")) document.getElementById("thursdayText").innerHTML="Donnerstag";
+    if (document.querySelector("#fridayText")) document.getElementById("fridayText").innerHTML="Freitag";
+    if (document.querySelector("#saturdayText")) document.getElementById("saturdayText").innerHTML="Samstag";
+    if (document.querySelector("#actionText")) document.getElementById("actionText").innerHTML="Erweiterte Funktionen";
+    if (document.querySelector("#ereaseText")) document.getElementById("ereaseText").innerHTML="Alles löschen";
+    if (document.querySelector("#ereaseDescText")) document.getElementById("ereaseDescText").innerHTML="Mit diesem Knopf wird die gesamte Tafel gesäubert.";
+    if (document.querySelector("#deleteText")) document.getElementById("deleteText").innerHTML="Letzten Eintrag löschen";
+    if (document.querySelector("#deleteDescText")) document.getElementById("deleteDescText").innerHTML="Mit diesem Knopf wird die zuletzt gesendete Nachricht gelöscht.";
+    if (document.querySelector("#howtoText")) document.getElementById("howtoText").innerHTML="Anleitung";
+    if (document.querySelector("#howdescText")) document.getElementById("howdescText").innerHTML=""+
         "👋&emsp;Willkommen beim Schwarzen Brett! Diese Software soll so einfach wie möglich sein. <br>"+
         "💌&emsp;Du hast eine Nachricht zu teilen? Super! Gib sie einfach ein und klick \"Senden\". <br>"+
 		"👀&emsp;Die Mitteilung wird sofort für jeden zu sehen sein.<br>"+
 		"🔧&emsp;Du kannst die Nachricht mit gedrückter Maustaste auf dem Bildschirm verschieben. <br>"+
 		"🗑️&emsp;Mit einem Doppelklick werden Nachrichten wieder entfernt.<br>"+
 		"📅&emsp;Wenn die Nachricht nicht manuell gelöscht wird, passiert das automatisch nach zwei Tagen.";
-    document.getElementById("startText").innerHTML="🚀&emsp;Das wars! Du kannst sofort loslegen!";
-    document.getElementById("moreText").innerHTML="Du willst mehr?";
-    document.getElementById("moredescText").innerHTML=""+
+    if (document.querySelector("#startText")) document.getElementById("startText").innerHTML="🚀&emsp;Das wars! Du kannst sofort loslegen!";
+    if (document.querySelector("#moreText")) document.getElementById("moreText").innerHTML="Du willst mehr?";
+    if (document.querySelector("#moredescText")) document.getElementById("moredescText").innerHTML=""+
         "🤹&emsp;Wenn Du noch mehr Einstellungen willst, hab ich da was für dich: <br>"+
         "🦚&emsp;Klick einfach auf \"Erweitert\" unterhalb des Senden-Knopfes um alle Einstellungen zu sehen<br>"+
         "📚&emsp;Wenn Du Hilfe brauchst, schau Dir die folgenden Artikel an.";
-    document.getElementById("scheduleTitleText").innerHTML="Nachrichten Planen";
-    document.getElementById("scheddescText").innerHTML="Dieser Artikel wird Dir zeigen, wie Du den Zeitraum deiner Nachrichten kontrollieren kannst.";
-    document.getElementById("learn1more").innerHTML="Mehr erfahren";
-    document.getElementById("timetableTitleText").innerHTML="Tagespläne bearbeiten";
-    document.getElementById("timetabledescText").innerHTML="Lerne wie Du die wöchentlichen Informationen des Tagesplans auf der rechten Seite bearbeitest.";
-    document.getElementById("learn2more").innerHTML="Mehr erfahren";
+    if (document.querySelector("#scheduleTitleText")) document.getElementById("scheduleTitleText").innerHTML="Nachrichten Planen";
+    if (document.querySelector("#scheddescText")) document.getElementById("scheddescText").innerHTML="Dieser Artikel wird Dir zeigen, wie Du den Zeitraum deiner Nachrichten kontrollieren kannst.";
+    if (document.querySelector("#learn1more")) document.getElementById("learn1more").innerHTML="Mehr erfahren";
+    if (document.querySelector("#timetableTitleText")) document.getElementById("timetableTitleText").innerHTML="Tagespläne bearbeiten";
+    if (document.querySelector("#timetabledescText")) document.getElementById("timetabledescText").innerHTML="Lerne wie Du die wöchentlichen Informationen des Tagesplans auf der rechten Seite bearbeitest.";
+    if (document.querySelector("#learn2more")) document.getElementById("learn2more").innerHTML="Mehr erfahren";
 }
